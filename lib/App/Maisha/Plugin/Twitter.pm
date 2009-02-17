a3651e9c5194bc2c8b198e7c03f6f3f041d019a3
barbie <barbie@barbie-laptop.(none)>
2009-02-17 15:46:16 +0000

    fixed version numbering

--- lib/App/Maisha/Plugin/Twitter.pm
+++ lib/App/Maisha/Plugin/Twitter.pm
@@ -3,7 +3,7 @@ package App::Maisha::Plugin::Twitter;
 use strict;
 use warnings;
 
-our $VERSION = '0.09';
+our $VERSION = '0.10';
 
 #----------------------------------------------------------------------------
 # Library Modules
