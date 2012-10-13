744b7b3bfdb4415b7d1d43b76825e55a18dd05da
Barbie <barbie@missbarbell.co.uk>
2012-10-13 21:40:32 +0100

    minor fixes

--- lib/App/Maisha.pm
+++ lib/App/Maisha.pm
@@ -180,13 +180,14 @@ Dave Cross, Robert Rothenberg and Steffen Müller.
 
 =head1 AUTHOR
 
-  Copyright (c) 2009-2010 Barbie <barbie@cpan.org> for Grango.org.
+  Barbie, <barbie@cpan.org>
+  for Miss Barbell Productions <http://www.missbarbell.co.uk>.
 
-=head1 LICENSE
+=head1 COPYRIGHT AND LICENSE
 
-  This program is free software; you can redistribute it and/or modify it
-  under the same terms as Perl itself.
+  Copyright (C) 2009-2012 by Barbie
 
-  See http://www.perl.com/perl/misc/Artistic.html
+  This module is free software; you can redistribute it and/or
+  modify it under the Artistic License v2.
 
 =cut
